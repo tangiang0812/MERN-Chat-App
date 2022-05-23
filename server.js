6357a85b451b1c07a5e7633b2d31b5ca64aa399e
Gnaig <tangiang0812@gmail.com>
2022-05-23 22:39:07 +0700

    basic server setup

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const rooms = ["general", "tech", "finance", "crypto"];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+const server = require("http").createServer(app);
+
+// const PORT = process.env.PORT || 8000;
+const PORT = 8000;
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http:/localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
